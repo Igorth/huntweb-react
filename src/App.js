50b11b28631c18328de4d41a5edfd764ca644e72
Igorth <igordiasth@gmail.com>
2020-03-04 19:27:17 +1300

    creating components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from 'react';
+import './styles.css';
+import Header from './components/Header/index';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello</h1>
+const App = () => (
+  <div className="App">
+      <Header />
     </div>
-  );
-}
+);
 
 export default App;
